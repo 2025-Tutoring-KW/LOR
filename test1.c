d2791ea1755060f44225f377f9a641cf3b4d8e4b
sirius 0512 <rising2005@outlook.com>
2025-05-17 22:54:35 +0900

    7주차 test

--- test1.c
+++ test1.c
@@ -16,17 +16,25 @@ int main(void) {
 int initialize(char*** arr) {
 	char tmp[20];
 	int num=0;
+	
 	while (1) {
+		int test = 0;
 		scanf("%s", &tmp);
 		if (tmp[0] == '0') {
 			return num;
 		}
 		else {
-			
-			*arr= (char**)realloc(*arr,sizeof(char*)*(num+1));
-			(*arr)[num] = (char*)malloc((strlen(tmp)+1)*sizeof(char));
-			strcpy((*arr)[num], &tmp);
-			num++;
+			for (int i = 0; i < num; i++) {
+				if (my_strcmp((*arr)[i], &tmp, num)==0 ) {
+					test++;
+				}
+			}
+			if (test == 0) {
+				*arr = (char**)realloc(*arr, sizeof(char*) * (num + 1));
+				(*arr)[num] = (char*)malloc((strlen(tmp) + 1) * sizeof(char));
+				strcpy((*arr)[num], &tmp);
+				num++;
+			}
 		}
 
 	}
@@ -37,23 +45,21 @@ int sort(char*** arr, int num) {
 	for (int i = 0; i < num - 1; i++) {
 		for (int j = i+1; j < num; j++) {
 
-			if (my_strcmp((*arr)[i],(*arr)[j],num)==1) {
+			if (my_strcmp((*arr)[i],(*arr)[j],num)>0) {
 				char* tmp = (*arr)[i];
 				(*arr)[i] = (*arr)[j];
 				(*arr)[j] = tmp;
 
-			}
-			else if (my_strcmp((*arr)[i], (*arr)[j], num) == -1) {
-				(*arr)[i][0] = '\0';
 			}
 		}	
 		
 	}
 }
 int my_strcmp(char* arr1, char* arr2, int num) {//같으면 -1 ,크면1이 크면 1,1이 작으면 0
-	for (int i = 0; i<strlen(arr1)&&i<strlen(arr2); i++) {
-		char a = (*arr1);
-		char b = (*arr2);
+	char a=100, b=100;
+	while (1){
+		 a = (*arr1);
+		 b = (*arr2);
 		if (a >= 'a') {
 			a=a - 32;
 		}
@@ -61,15 +67,15 @@ int my_strcmp(char* arr1, char* arr2, int num) {//같으면 -1 ,크면1이 크면 1,1이 
 			b=b - 32;
 		}
 		if (a == b) {
-			if (i == strlen(arr1)-1 || i == strlen(arr2)-1) {
-				if (strlen(arr1) == strlen(arr2)) {
-					return -1;
-				}
-				else if (strlen(arr1) >= strlen(arr2)) {
+			if (a==0||b==0) {
+				if (a == 0 && b == 0) {
 					return 0;
 				}
-				else if (strlen(arr1) <= strlen(arr2)) {
+				else if (a == 0) {
 					return 1;
+				}
+				else if (b == 0) {
+					return -1;
 				}
 			}
 			else {
@@ -78,12 +84,10 @@ int my_strcmp(char* arr1, char* arr2, int num) {//같으면 -1 ,크면1이 크면 1,1이 
 				continue;
 			}
 		}
-		else if (a < b) {
-			return 0;
+		else if (a!=b) {
+			return  a-b;
 		}
-		else {
-			return 1;
-		}
+		
 	}
 }
 int end(char*** arr,int num) {
